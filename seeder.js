93451bda873cf7348605467cd892dc7129794706
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2024-01-05 23:18:46 +0400

    add Course schema

--- seeder.js
+++ seeder.js
@@ -1,46 +1,51 @@
-import fs from "fs";
-import mongoose from "mongoose";
-import colors from "colors";
-import dotenv from "dotenv";
-import Bootcamp from "./models/Bootcamp.js";
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
+import fs from 'fs'
+import mongoose from 'mongoose'
+import colors from 'colors'
+import dotenv from 'dotenv'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+import Bootcamp from './models/Bootcamp.js'
+import Course from './models/Course.js'
 
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: './config/config.env' })
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI)
 
 const bootcamps = JSON.parse(
-  fs.readFileSync(resolve(__dirname, "_data", "bootcamps.json"), "utf-8")
-);
+  fs.readFileSync(resolve(__dirname, '_data', 'bootcamps.json'), 'utf-8')
+)
+
+const courses = JSON.parse(fs.readFileSync(resolve(__dirname, '_data', 'courses.json'), 'utf-8'))
 
 const importData = async () => {
   try {
-    await Bootcamp.create(bootcamps);
+    await Bootcamp.create(bootcamps)
+    await Course.create(courses)
 
-    console.log("Data imported...".green.inverse);
-    process.exit();
+    console.log('Data imported...'.green.inverse)
+    process.exit()
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
-};
+}
 
 const deleteData = async () => {
   try {
-    await Bootcamp.deleteMany();
+    await Bootcamp.deleteMany()
+    await Course.deleteMany()
 
-    console.log("Data deleted...".red.inverse);
-    process.exit();
+    console.log('Data deleted...'.red.inverse)
+    process.exit()
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
-};
+}
 
-if (process.argv[2] === "-i") {
-  importData();
-} else if (process.argv[2] === "-d") {
-  deleteData();
+if (process.argv[2] === '-i') {
+  importData()
+} else if (process.argv[2] === '-d') {
+  deleteData()
 }
