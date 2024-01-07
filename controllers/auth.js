1991fb08927599625338e938613cfb00206bef58
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2024-01-07 18:28:55 +0400

    hash password before saving new user to the database

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,17 @@
 import Bootcamp from '../models/Bootcamp.js'
 import { ErrorResponse } from '../utils/errorResponse.js'
 import { asyncHandler } from '../middleware/async.js'
+import User from '../models/User.js'
 
 export const register = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  })
+
   res.status(200).json({ success: true })
 })
