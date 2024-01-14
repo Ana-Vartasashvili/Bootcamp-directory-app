a22197161c95b9427bc3b6660d787cdb36fcb4d1
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2024-01-14 15:53:31 +0400

    add functionality to delete and update a review

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -40,3 +40,38 @@ export const addReview = asyncHandler(async (req, res, next) => {
 
   res.status(201).json({ success: true, data: review })
 })
+
+export const updateReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id)
+
+  if (!review) {
+    return next(new ErrorResponse(`No review with the id of ${req.params.id}`, 404))
+  }
+
+  if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse('Not authorized to update review', 401))
+  }
+
+  const updatedReview = await Review.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  })
+
+  res.status(201).json({ success: true, data: updatedReview })
+})
+
+export const deleteReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id)
+
+  if (!review) {
+    return next(new ErrorResponse(`No review with the id of ${req.params.id}`, 404))
+  }
+
+  if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse('Not authorized to update review', 401))
+  }
+
+  await Review.findByIdAndDelete(req.params.id)
+
+  res.status(201).json({ success: true, data: {} })
+})
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,11 @@
 import express from 'express'
-import { addReview, getReview, getReviews } from '../controllers/reviews.js'
+import {
+  addReview,
+  deleteReview,
+  getReview,
+  getReviews,
+  updateReview,
+} from '../controllers/reviews.js'
 import Review from '../models/Review.js'
 import { advancedResults } from '../middleware/advancedResults.js'
 import { protect, authorize } from '../middleware/auth.js'
@@ -17,6 +23,10 @@ router
   )
   .post(protect, authorize('user', 'admin'), addReview)
 
-router.route('/:id').get(getReview)
+router
+  .route('/:id')
+  .get(getReview)
+  .put(protect, authorize('user', 'admin'), updateReview)
+  .delete(protect, authorize('user', 'admin'), deleteReview)
 
 export default router
