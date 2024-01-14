52669db8c76caf6fbc4bcb0982e4921b7edb6c86
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2024-01-14 12:26:14 +0400

    add user details and password update functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -111,3 +111,30 @@ const sendTokenResponse = (user, statusCode, res) => {
     token,
   })
 }
+
+export const updateUserDetails = asyncHandler(async (req, res, next) => {
+  const fieldsToUpdate = { name: req.body.name, email: req.body.email }
+
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true,
+  })
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  })
+})
+
+export const updatePassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select('+password')
+
+  if (!(await user.matchPassword(req.body.currentPassword))) {
+    return next(new ErrorResponse('Password is incorrect', 401))
+  }
+
+  user.password = req.body.newPassword
+  await user.save()
+
+  sendTokenResponse(user, 200, res)
+})
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,21 @@
 import express from 'express'
-import { register, login, forgotPassword, resetPassword } from '../controllers/auth.js'
+import {
+  register,
+  login,
+  forgotPassword,
+  resetPassword,
+  updateUserDetails,
+  updatePassword,
+} from '../controllers/auth.js'
 import { protect } from '../middleware/auth.js'
 
 const router = express.Router()
 
 router.route('/register').post(register)
 router.route('/login').post(login)
+router.route('/updateDetails').put(protect, updateUserDetails)
 router.route('/forgotPassword').post(forgotPassword)
 router.route('/resetPassword/:resetToken').put(resetPassword)
+router.route('/updatePassword').put(protect, updatePassword)
 
 export default router
